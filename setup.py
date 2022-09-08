54e6d1dadad3a52a4d185d97f5468f7812d86961
Muhammad Sufyan <muhammad.sufyan@apimatic.io>
2022-09-09 01:48:54 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ else:
 
 setup(
     name='wordsapi',
-    version='1.0.2',
+    version='1.0.3',
     description='Python client library for Words API',
     long_description=long_description,
     long_description_content_type="text/markdown",
