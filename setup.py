a107c404efc2ed9cd1e4cf1f4805a2ff1c4f4316
Muhammad Sufyan <muhammad.sufyan@apimatic.io>
2022-09-08 20:09:10 +0000

    Adds the SDK in the repository

---
+++ setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import sys
+from setuptools import setup, find_packages
+
+if sys.version_info[0] < 3:
+    with open('README.md', 'r') as fh:
+        long_description = fh.read()
+else:
+    with open('README.md', 'r', encoding='utf-8') as fh:
+        long_description = fh.read()
+
+setup(
+    name='wordsapi',
+    version='1.0.2',
+    description='Python client library for Words API',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Muhammad Sufyan',
+    author_email='muhammad.sufyan@apimatic.io',
+    url='apimatic.io',
+    packages=find_packages(),
+    install_requires=[
+        'jsonpickle~=1.4, >= 1.4.1',
+        'requests~=2.25',
+        'cachecontrol~=0.12.6',
+        'python-dateutil~=2.8.1',
+        'enum34~=1.1, >=1.1.10'
+    ],
+    tests_require=[
+        'nose>=1.3.7'
+    ],
+    test_suite = 'nose.collector'
+)
---
+++ wordsapi/models/__init__.py
@@ -0,0 +1,12 @@
+__all__ = [
+    'definitions_response',
+    'examples_response',
+    'pronunciation_details',
+    'synonyms_response',
+    'frequency_response',
+    'word_details',
+    'word_response',
+    'pronunciation_response',
+    'frequency_details',
+    'syllable_details',
+]
